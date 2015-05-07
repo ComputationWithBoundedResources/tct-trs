081775f29a5c4c1491efe5a0a38efd892c1a1ab0
mschaper <michael.schaper@uibk.ac.at>
2015-05-07 16:26:46 +0200

    included proofoutput in cmd line options

--- src/Tct/Trs/Data/Mode.hs
+++ src/Tct/Trs/Data/Mode.hs
@@ -1,19 +1,19 @@
 module Tct.Trs.Data.Mode
   (
-  Problem (..)
-  , TrsMode
+    TrsMode
   , trsMode
-  , CC
+  , TrsOptions
   ) where
 
 
+import           Control.Applicative
 import qualified Data.Set                   as S
 
 import           Tct.Core.Common.Error      (TctError (..))
 import qualified Tct.Core.Common.Pretty     as PP
 import qualified Tct.Core.Common.Xml        as Xml (putXml)
+import qualified Tct.Core.Data              as T
 import           Tct.Core.Main
-import qualified Tct.Core.Data as T
 import           Tct.Core.Processor.Failing (failing)
 
 import qualified Data.Rewriting.Problem     as R
@@ -27,7 +27,7 @@ import qualified Tct.Trs.Data.Trs           as Trs
 import           Tct.Trs.Processor          (defaultDeclarations)
 
 
-type TrsMode = TctMode TrsProblem TrsProblem CC
+type TrsMode = TctMode TrsProblem TrsProblem TrsOptions
 
 trsMode :: TrsMode
 trsMode = TctMode
@@ -40,41 +40,83 @@ trsMode = TctMode
   , modeModifyer        = modifyer
   , modeAnswer          = answering }
 
--- TODO: MS: options for total or partial proof output
-answering :: CC -> T.Return (ProofTree TrsProblem) -> IO ()
-answering _ ret = case ret of
-  T.Flop -> putStrLn "Flop`"
-  r    -> PP.putPretty (answer pt) >> case totalProof pt of
-    Left s    -> print s
-    Right xml -> Xml.putXml xml
-    where pt = T.fromReturn r
-
-data CC = DCF | DCI | RCF | RCI deriving (Eq, Show, Read)
-
-ccProperties :: CC -> Trs F V -> Signature F -> (StartTerms F, Strategy)
-ccProperties cc trs sig = case cc of
-  DCF -> (AllTerms fs,          Full)
-  DCI -> (AllTerms fs,          Innermost)
-  RCF -> (BasicTerms defs cons, Full)
-  RCI -> (BasicTerms defs cons, Innermost)
-  where
-    fs   = defs `S.union` cons
-    defs = Trs.definedSymbols trs
-    cons = Trs.constructorSymbols sig defs
-
+-- | WST format parser from 'Data.Rewriting'.
 parser :: String -> Either TctError TrsProblem
 parser s = case R.fromString s of
   Left e  -> Left $ TctParseError (show e)
   Right p -> Right $ fromRewriting p
 
-options :: Options CC
-options = option $ eopt
-  `withArgLong` "complexity"
-  `withCropped` 'c'
-  `withHelpDoc` PP.text "RCF - runtime complexity"
-  `withDefault` RCF
+-- | Trs specific command line options.
+options :: Options TrsOptions
+options = TrsOptions
+  <$> option' readCC (eopt
+    `withArgLong` "complexity"
+    `withCropped` 'c'
+    `withHelpDoc` PP.listing
+      [ (PP.text "dc"  , PP.text "derivational complexity")
+      , (PP.text "dci" , PP.text "derivational complexity innermost")
+      , (PP.text "rc"  , PP.text "runtime complexity")
+      , (PP.text "rci" , PP.text "runtime complexity innermost") ]
+    `withDefault` RCI)
+  <*> option' readCP (eopt
+      `withArgLong` "proofOutput"
+      `withCropped` 'p'
+      `withHelpDoc` PP.listing
+        [ (PP.text "totalProof"   , PP.text "outputs the answer and then the CeTA proof")
+        , (PP.text "partialProof" , PP.text "outputs the answer and then the CeTA (partial) proof") ]
+      `withDefault` TotalProof)
 
-modifyer :: CC -> TrsProblem -> TrsProblem
-modifyer cc p = p { startTerms = ts, strategy = st }
+-- | Sets complexity problem.
+modifyer :: TrsOptions -> TrsProblem -> TrsProblem
+modifyer (TrsOptions cc _) p = p { startTerms = ts, strategy = st }
   where (ts,st) = ccProperties cc (allComponents p) (signature p)
+
+-- | CeTA (partial proof output)
+answering :: TrsOptions -> T.Return (ProofTree TrsProblem) -> IO ()
+answering (TrsOptions _ cp) ret = case ret of
+  T.Halt _  -> PP.putPretty T.MaybeAnswer
+  r         -> PP.putPretty (answer pt) >> case prover pt of
+    Left s    -> print s
+    Right xml -> Xml.putXml xml
+    where
+      pt = T.fromReturn r
+      prover = if cp == TotalProof then totalProof else partialProof
+
+
+data TrsOptions =  TrsOptions CC CP
+
+data CC = DC | DCI | RC | RCI deriving Eq
+
+instance Show CC where
+  show cc = case cc of { DC -> "dc"; DCI -> "dci"; RC -> "rc"; RCI -> "rci" }
+
+readCC :: Monad m => String -> m CC
+readCC cc
+  | cc == show DC  = return DC
+  | cc == show DCI = return DCI
+  | cc == show DC  = return DC
+  | cc == show DCI = return DCI
+  | otherwise      =  fail $ "Tct.Trs.Data.Mode.readCC: " ++ cc
+
+data CP = TotalProof | PartialProof deriving Eq
+
+instance Show CP where
+  show cc = case cc of { TotalProof -> "totalProof"; PartialProof -> "partialProof" }
+
+readCP :: Monad m => String -> m CP
+readCP cp
+  | cp == show TotalProof   = return TotalProof
+  | cp == show PartialProof = return PartialProof
+  | otherwise               = fail $ "Tct.Trs.Data.Mode:" ++ cp
+
+ccProperties :: CC -> Trs F V -> Signature F -> (StartTerms F, Strategy)
+ccProperties cc trs sig = case cc of
+  DC  -> (AllTerms fs,          Full)
+  DCI -> (AllTerms fs,          Innermost)
+  RC  -> (BasicTerms defs cons, Full)
+  RCI -> (BasicTerms defs cons, Innermost)
+  where
+    fs   = defs `S.union` cons
+    defs = Trs.definedSymbols trs
+    cons = Trs.constructorSymbols sig defs
 
