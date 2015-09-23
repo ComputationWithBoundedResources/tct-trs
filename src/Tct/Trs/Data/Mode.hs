af032b7c135e1fd65e43d27b0b7d509d380dbef9
mschaper <michael.schaper@uibk.ac.at>
2015-09-23 16:09:41 +0200

    adapt configuration to tct-core 3.1
      - set up a `trsConfiguration` that is compatible with tct-core-3.1
      - provide a dedicate entry point `trs` that only requires
        a `trsConfiguration`

--- src/Tct/Trs/Data/Mode.hs
+++ src/Tct/Trs/Data/Mode.hs
@@ -1,8 +1,27 @@
+{- | This module provides a Tct configuration for Trs problems.
+
+The configuration is meant to work with 'Tct.Core.Main.tct3'
+
+  * sets the parser to standard @xml@ / @wst@ format
+  * sets default strategies
+  * provides the @--complexity <rc|rci|dc|dci>@ argument to set the complexity problem
+    * this argument overrides the complexity problem returned by the parser
+  * provides @--ceta <total|partial>@ argument to set the complexity problem
+-}
 module Tct.Trs.Data.Mode
   (
-    TrsMode
-  , trsMode
+  trs
+  -- * Problem parser
+  , parserIO
+  , parser
+  -- * Trs Configuration
+  , TrsConfig
+  , trsConfig
+  -- * Trs Options
   , TrsOptions
+  , trsOptions
+  -- * Trs update hook
+  , trsUpdate
   ) where
 
 
@@ -13,78 +32,63 @@ import qualified Tct.Core.Common.Pretty     as PP
 import qualified Tct.Core.Common.Xml        as Xml (putXml)
 import qualified Tct.Core.Data              as T
 import           Tct.Core.Main
-import           Tct.Core.Processor.Failing (failing)
+import           Tct.Core
 
 import qualified Data.Rewriting.Problem     as R
 import qualified Data.Rewriting.Problem.Xml as R
 
 import           Tct.Trs.Data.CeTA
 import           Tct.Trs.Data.Problem
-import           Tct.Trs.Processor          (defaultDeclarations)
+import           Tct.Trs.Declarations        (trsDeclarations)
 
 
-type TrsMode = TctMode TrsProblem TrsProblem TrsOptions
+trs :: TrsConfig -> IO ()
+trs = tct3WithOptions trsUpdate trsOptions
 
-trsMode :: TrsMode
-trsMode = TctMode
-  { modeId              = "trs"
-  , modeParser          = parserIO
-  , modeStrategies      = defaultDeclarations
-
-  , modeDefaultStrategy = failing
-  , modeOptions         = options
-  , modeModifyer        = modifyer
-  , modeAnswer          = \_ _  -> return ()
-  , modeProof           = proofing }
-
--- | Parses a TrsProblem. Uses the @xml@ format if the file extension is @xml@, otherwise the WST format.
+-- | Parses a TrsProblem. Uses the @xml@ format if the file extension is @xml@, otherwise the @WST@ format.
 parserIO :: FilePath -> IO (Either String TrsProblem)
-parserIO fn 
+parserIO fn
   | takeExtension fn == ".xml" = fromRewriting <$> R.xmlFileToProblem fn
   | otherwise                  = parser <$> readFile fn
 
--- | WST format parser from 'Data.Rewriting'.
+-- | @WST@ format parser from 'Data.Rewriting'.
 parser :: String -> Either String TrsProblem
 parser s = case R.fromString s of
   Left e  -> Left (show e)
   Right p -> fromRewriting p
 
+
+-- | The Tct configuration type for Trs.
+type TrsConfig = TctConfig TrsProblem
+
+-- | Default Tct configuration for Trs.
+-- Sets the @xml@ / @wst@ parser. Sets a list of default strategies.
+trsConfig :: TrsConfig
+trsConfig = defaultTctConfig parserIO
+  `addStrategies` trsDeclarations
+  `appendGHCiScript`
+    [ ":module +Tct.Trs.Processors"
+    , ":module +Tct.Trs.Interactive"]
+
+-- | Trs specific command line arguments.
+data TrsOptions =  TrsOptions (Maybe CC) CP
+
 -- | Trs specific command line options.
-options :: Options TrsOptions
-options = TrsOptions
+trsOptions :: Options TrsOptions
+trsOptions = TrsOptions
   <$> alt (option' readCC (eopt
     `withArgLong` "complexity"
-    `withCropped` 'c'
     `withHelpDoc` PP.listing
       [ (PP.text (show DC)  , PP.text "derivational complexity")
       , (PP.text (show DCI) , PP.text "derivational complexity innermost")
       , (PP.text (show RC)  , PP.text "runtime complexity")
       , (PP.text (show RCI) , PP.text "runtime complexity innermost") ] ))
   <*> option' readCP (eopt
-      `withArgLong` "proofOutput"
-      `withCropped` 'b'
+      `withArgLong` "ceta"
       `withHelpDoc` PP.listing
-        [ (PP.text (show TotalProof)   , PP.text "outputs the answer and then the CeTA proof")
-        , (PP.text (show PartialProof) , PP.text "outputs the answer and then the CeTA (partial) proof") ]
+        [ (PP.text (show TotalProof)   , PP.text "outputs the proof in the CeTA format")
+        , (PP.text (show PartialProof) , PP.text "outputs the proof in the CeTA (partial) format") ]
       `withDefault` TotalProof)
-
--- | Sets complexity problem.
-modifyer :: TrsOptions -> TrsProblem -> TrsProblem
-modifyer (TrsOptions cc _) = updateCC cc
-
--- | CeTA (partial proof output)
-proofing :: TrsOptions -> T.Return (ProofTree TrsProblem) -> IO ()
-proofing (TrsOptions _ cp) ret = case ret of
-  T.Halt _  -> PP.putPretty $ PP.text "MAYBE"
-  r         -> case prover pt of
-    Left s    -> print s
-    Right xml -> Xml.putXml xml
-    where
-      pt     = T.fromReturn r
-      prover = if cp == TotalProof then totalProof else partialProof
-
-
-data TrsOptions =  TrsOptions (Maybe CC) CP
 
 data CC = DC | DCI | RC | RCI deriving Eq
 
@@ -102,7 +106,7 @@ readCC cc
 data CP = TotalProof | PartialProof deriving Eq
 
 instance Show CP where
-  show cc = case cc of { TotalProof -> "totalProof"; PartialProof -> "partialProof" }
+  show cc = case cc of { TotalProof -> "total"; PartialProof -> "partial" }
 
 readCP :: Monad m => String -> m CP
 readCP cp
@@ -110,10 +114,28 @@ readCP cp
   | cp == show PartialProof = return PartialProof
   | otherwise               = fail $ "Tct.Trs.Data.Mode:" ++ cp
 
-updateCC :: Maybe CC -> TrsProblem -> TrsProblem
-updateCC cc = case cc of
-  Nothing    -> id
-  (Just DC)  -> toDC . toFull
-  (Just DCI) -> toDC . toInnermost
-  (Just RC)  -> toRC . toFull
-  (Just RCI) -> toRC . toInnermost
+
+-- | Update hook for options
+-- Updates the parsed problem if a @complexity@ argument is set.
+-- Updates the proof output if the @proofOUtput@ argument is set.
+trsUpdate :: TrsConfig -> TrsOptions -> TrsConfig
+trsUpdate cfg (TrsOptions ccM cp) = setParseProblem $ setPutProof cfg
+  where
+    setParseProblem cfg' = cfg' { parseProblem = \fp -> fmap (updateCC ccM) <$> parseProblem cfg' fp }
+    setPutProof cfg'     = cfg' { putProof     = proofing }
+
+    updateCC cc = case cc of
+      Nothing    -> id
+      (Just DC)  -> toDC . toFull
+      (Just DCI) -> toDC . toInnermost
+      (Just RC)  -> toRC . toFull
+      (Just RCI) -> toRC . toInnermost
+    proofing ret = case ret of
+      T.Halt _  -> PP.putPretty $ PP.text "MAYBE"
+      r         -> case prover pt of
+        Left s    -> print s
+        Right xml -> Xml.putXml xml
+        where
+          pt     = T.fromReturn r
+          prover = if cp == TotalProof then totalProof else partialProof
+
