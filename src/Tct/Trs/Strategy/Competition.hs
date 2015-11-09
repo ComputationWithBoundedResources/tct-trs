bcbfa590a33e167e8f4f50df4b287f0838aea63f
mschaper <michael.schaper@uibk.ac.at>
2015-11-09 14:55:41 +0100

    move competition strategy into extra file

---
+++ src/Tct/Trs/Strategy/Competition.hs
@@ -0,0 +1,32 @@
+-- | The competition strategy. Wrapper for default runtime/derivational complexity.
+module Tct.Trs.Strategy.Competition
+  ( competition
+  , competitionDeclaration
+  ) where
+
+
+import           Tct.Core
+import           Tct.Core.Data                 (deflFun)
+
+import           Tct.Trs.Data
+import           Tct.Trs.Data.Problem          (isRCProblem)
+import           Tct.Trs.Processors
+import           Tct.Trs.Strategy.Derivational
+import           Tct.Trs.Strategy.Runtime
+
+
+-- | Declaration for "competition" strategy.
+competitionDeclaration :: Declaration ('[] :-> TrsStrategy)
+competitionDeclaration = strategy "competition" () competitionStrategy
+
+-- | Default competition strategy.
+competition :: TrsStrategy
+competition = deflFun competitionDeclaration
+
+competitionStrategy :: TrsStrategy
+competitionStrategy =
+  withProblem $ \p ->
+    if isRCProblem p
+      then runtime' Best
+      else derivational
+
